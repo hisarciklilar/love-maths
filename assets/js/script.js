47281a8f6b5d0fcb3e706b83793116f06ad88d68
Mehtap Hisarciklilar <hisarciklilar@gmail.com>
2024-10-20 22:38:34 +0100

    Add code to calculate score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,8 +53,10 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert("Hey! You got it right! :D"); 
+    incrementScore();
   } else {
     alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}`);
+    incrementWrongAnswer();
   }
 
   runGame(calculatedAnswer[1]);
@@ -77,12 +79,20 @@ function calculatedCorrectAnswer() {
   }
 }
 
+/**
+ * Gets the correct score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+  let oldScore = parseInt(document.getElementById('score').innerText);
+  document.getElementById('score').innerText = ++oldScore; 
 }
 
+/**
+ * Gets the correct tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+  let oldScore = parseInt(document.getElementById('incorrect').innerText);
+  document.getElementById('incorrect').innerText = ++oldScore; 
 }
 
 function displayAdditionQuestion(operand1, operand2) {
